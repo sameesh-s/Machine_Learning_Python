4a6824983c949965aebce16d23effcc697e00233
sameesh-s <sameesh.fisat@gmail.com>
2016-02-02 12:45:27 +0530

    nighbours listing added

--- knn_algorithm/sample.py
+++ knn_algorithm/sample.py
@@ -5,9 +5,11 @@ def euclideanDistance(instance1, instance2, length):
 	for x in range(length):
 		distance += pow((instance1[x] - instance2[x]), 2)
 	return math.sqrt(distance)
+
 def most_common(lst):
     return max(set(lst), key=lst.count)
 
+print "Enter 3 points seperated by white space"
 num = raw_input().split()
 num = [int(x) for x in num]
 
@@ -15,37 +17,39 @@ with open("dataset.txt") as textFile:
 	lines = [line.split(',') for line in textFile]
 length=lines.__len__()
 
-n = 3#number of nighbours
+print "Enter number of nighbours : "
+n=input();
 
-neighbour = [[89563245.0 for x in range(n)] for x in range(2)]
-top = 2
-
-print neighbour
+neighbour = [[89563245.0 for x in range(n)] for x in range(3)]
+top = n - 1
 
 print "Length of training set :"+ str(length)
 for i in range(len(lines)):
 	lines[i][3]=lines[i][3].strip('\n')
 	lines[i] = [int(numeric_string) for numeric_string in lines[i]]
 	print lines[i]
-	print "Distance class"
 	distance = euclideanDistance(num, lines[i], 3)
 	clas=lines[i][3]
-	print distance,clas
-	top = 2
-	print top
-	print distance <=neighbour[0][top] and top > -1
+	point = i
+	top = n - 1
 	while ( distance <= neighbour[0][top] and top > -1 ):
 		top=top-1
 	top=top+1
 	while(top < n):
 		#swap element
-		tempD=neighbour[0][top]
-		tempC=neighbour[1][top]
-		neighbour[0][top]=distance
-		neighbour[1][top]=clas
-		distance=tempD
-		clas=tempC
+		tempD = neighbour[0][top]
+		tempC = neighbour[1][top]
+		tempP = neighbour[2][top]
+		neighbour[0][top] = distance
+		neighbour[1][top] = clas
+		neighbour[2][top] = point
+		distance = tempD
+		clas = tempC
+		point = tempP
 		top = top + 1
-		print "inside while"
-		print neighbour
-print most_common(neighbour[1])
+		
+print "Belonging class : ",most_common(neighbour[1])
+print "Neighbours : "
+for i in range(len(neighbour[2])):
+	print lines[ neighbour[2][i] ]
+	
